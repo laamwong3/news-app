7414a53a7e52bdc7a01245291e5f901dab8117d7
Laam Wong <laamwong3@gmail.com>
2022-07-22 07:55:44 +0800

    create select box

--- components/home/CategorySelect.tsx
+++ components/home/CategorySelect.tsx
@@ -3,13 +3,34 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 
+interface CategoryType {
+  category:
+    | "business"
+    | "entertainment"
+    | "general"
+    | "health"
+    | "science"
+    | "sports"
+    | "technology";
+}
+
+const categories: CategoryType[] = [
+  { category: "business" },
+  { category: "entertainment" },
+  { category: "general" },
+  { category: "health" },
+  { category: "science" },
+  { category: "sports" },
+  { category: "technology" },
+];
+
 export default function CategorySelect() {
   return (
     <Autocomplete
       sx={{ width: 300 }}
-      options={countries}
+      options={categories}
       autoHighlight
-      getOptionLabel={(option) => option.label}
+      getOptionLabel={(option) => option.category.toUpperCase()}
       renderInput={(params) => (
         <TextField {...params} label="Choose a category" />
       )}
